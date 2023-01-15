3921b517930c300a3b3ddb43c6b2aabf74f03b34
thayrov <thayrovg@gmail.com>
2023-01-15 15:57:02 -0600

    organiza codigo js en distintos archivos para modular el proyecto y mejorar lectura; corrige enlances a scripts en archivos html

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -1,35 +1,38 @@
-const contar = entradas => entradas++;
-const calcEntryNo = Date.now();
-const calcUnits = (capital, price) => capital / price;
+//  Funciones
+const calcUnits = (balance, price) => balance / price;
 const calcSize = (price, units) => price * units;
 const calcCommission = (spread, units) => spread * units;
 const calcStopLoss = (position, price, change) => {
-	position == 'long'
-		? price * (1 - change * 0.382)
-		: price * (1 + change * 0.382);
+	switch (position) {
+		case 'long':
+			return price * (1 - (change / 100) * 0.382);
+		case 'short':
+			return price * (1 + (change / 100) * 0.382);
+	}
 };
-const calcTakeProfit = (position, price, change) => {
-	position == 'long'
-		? price * (1 + change * rrr * 0.382)
-		: price * (1 - change * rrr * 0.382);
+const calcTakeProfit = (position, price, change, rrr) => {
+	switch (position) {
+		case 'long':
+			return price * (1 + (change / 100) * rrr * 0.382);
+		case 'short':
+			return price * (1 - (change / 100) * rrr * 0.382);
+	}
 };
 const calcLossTotal = (position, units, stopLoss, price, commission) => {
-	position == 'long'
-		? units * (stopLoss - price) - commission
-		: units * (price - stopLoss) - commission;
+	switch (position) {
+		case 'long':
+			return units * (stopLoss - price) - commission;
+		case 'short':
+			return units * (price - stopLoss) - commission;
+	}
 };
 const calcProfitTotal = (position, units, takeProfit, price, commission) => {
-	position == 'long'
-		? units * (price - takeProfit) - commission
-		: units * (takeProfit - price) - commission;
+	switch (position) {
+		case 'long':
+			return units * (takeProfit - price) - commission;
+		case 'short':
+			return units * (price - takeProfit) - commission;
+	}
 };
 const calcRRRTotal = (profitTotal, lossTotal) => -profitTotal / lossTotal;
-const calcRisk = (lossTotal, capital) => (lossTotal / capital) * 100;
-
-const guardarEntradaLS = entradas => {
-	localStorage.setItem('entradas', JSON.stringify(entradas));
-};
-
-const cargarEntradaLS = () => {
-	return JSON.parse(localStorage.getItem('entradas')) || [];
-};
+const calcRisk = (lossTotal, balance) => (-lossTotal / balance) * 100;
--- scripts/variables.js
+++ scripts/variables.js
@@ -1,23 +1,92 @@
+//  Variables, declaraciones y clase constructora
 const sixDecimals = 6;
 const twoDecimals = 2;
-
 const entradas = [];
 
-let entryNo;
-let balance = document.getElementById('balance');
-let market = document.getElementById('instrumento');
-let position = document.getElementById('direccion');
-let date = document.getElementById('fecha');
-let price = document.getElementById('precio');
-let units;
-let spread = document.getElementById('spread');
-let size;
-let commission;
-let change = document.getElementById('variacion');
-let rrr = document.getElementById('rrr');
-let stopLoss;
-let takeProfit;
-let lossTotal;
-let profitTotal;
-let rrrTotal;
-let risk;
+let balance = document.getElementById('balance').value;
+let market = document.getElementById('instrumento').value;
+let position = document.getElementById('direccion').value;
+let date = document.getElementById('fecha').value;
+let price = document.getElementById('precio').value;
+let units = calcUnits(balance, price);
+let spread = document.getElementById('spread').value;
+let size = calcSize(price, units);
+let commission = calcCommission(spread, units);
+let change = document.getElementById('variacion').value;
+let rrr = document.getElementById('rrr').value;
+let stopLoss = calcStopLoss(position, price, change);
+let takeProfit = calcTakeProfit(position, price, change, rrr);
+let lossTotal = calcLossTotal(position, units, stopLoss, price, commission);
+let profitTotal = calcProfitTotal(
+	position,
+	units,
+	takeProfit,
+	price,
+	commission,
+);
+let rrrTotal = calcRRRTotal(profitTotal, lossTotal);
+let risk = calcRisk(lossTotal, balance);
+
+class EntradaMercado {
+	constructor(
+		date,
+		balance,
+		market,
+		position,
+		price,
+		units,
+		spread,
+		size,
+		commission,
+		change,
+		ratio,
+		stopLoss,
+		takeProfit,
+		lossTotal,
+		profitTotal,
+		ratio2,
+		risk,
+	) {
+		this.date = date;
+		this.balance = balance;
+		this.market = market;
+		this.position = position;
+		this.price = price;
+		this.units = units;
+		this.spread = spread;
+		this.size = size;
+		this.commission = commission;
+		this.change = change;
+		this.ratio = ratio;
+		this.stopLoss = stopLoss;
+		this.takeProfit = takeProfit;
+		this.lossTotal = lossTotal;
+		this.profitTotal = profitTotal;
+		this.ratio2 = ratio2;
+		this.risk = risk;
+	}
+
+	imprimirEntrada() {
+		return (document.getElementById('entradasCalculadora').innerHTML += `
+            <th scope="row">${this.date}</th>
+            <td>$${this.balance}</td>
+            <td>${this.market}</td>
+            <td>${this.position}</td>
+            <td>$${this.price}</td>
+            <td>${this.units}</td>
+            <td>$${this.spread}</td>
+            <td>$${this.size}</td>
+            <td>$${this.commission}</td>
+            <td>${this.change}%</td>
+            <td>${this.ratio}</td>
+            <td>$${this.stopLoss}</td>
+            <td>$${this.takeProfit}</td>
+            <td>$${this.lossTotal}</td>
+            <td>$${this.profitTotal}</td>
+            <td>${this.ratio2}</td>
+            <td>${this.risk}%</td>
+            </tr>
+            
+`);
+	}
+}
