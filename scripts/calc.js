890b6408605424b8241f3d734a63904576cd0fa1
thayrov <thayrovg@gmail.com>
2023-01-28 18:03:43 -0600

    vincula librería sweetalert a proyecto y agrega alert de prueba

--- scripts/calc.js
+++ scripts/calc.js
@@ -36,8 +36,7 @@ const addEntrada = () => {
 	entrada.imprimirEntrada();
 	//  Guardar datos en array
 	entradas.push(entrada);
-	//  Guardar array en local storage
-	localStorage.setItem('listaEntradas', JSON.stringify(entradas));
+
 	//  Limpiar formulario para siguiente entrada
 	document.querySelector('form').reset();
 };
@@ -79,7 +78,28 @@ const ejecutarImpresion = () => {
 })();
 
 const borrarTabla = () => {
-	document.getElementById('tabla').innerHTML = ``;
+	Swal.fire({
+		title: 'Hey, we have something to say!',
+		confirmButtonText: "Let's do this",
+		showCancelButton: false,
+		icon: 'info',
+		iconHtml:
+			'<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-bilibili" width="150" height="150" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M3 10a4 4 0 0 1 4 -4h10a4 4 0 0 1 4 4v6a4 4 0 0 1 -4 4h-10a4 4 0 0 1 -4 -4v-6zm5 -7l2 3m6 -3l-2 3m-7 6l3 -1m4 0l3 1m-8 3a1.625 1.625 0 0 0 3 0a1.625 1.625 0 0 0 3 0"></path></svg>',
+		text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+	});
+	document.getElementById(
+		'tabla',
+	).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-face-id-error" width="240" height="240" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+	<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+	<path d="M4 8v-2a2 2 0 0 1 2 -2h2"></path>
+	<path d="M4 16v2a2 2 0 0 0 2 2h2"></path>
+	<path d="M16 4h2a2 2 0 0 1 2 2v2"></path>
+	<path d="M16 20h2a2 2 0 0 0 2 -2v-2"></path>
+	<path d="M9 10h.01"></path>
+	<path d="M15 10h.01"></path>
+	<path d="M9.5 15.05a3.5 3.5 0 0 1 5 0"></path>
+</svg>
+<h2>Aún no se han agregado entradas</h2>`;
 };
 
 //  Evento para activar función borrarTabla()
@@ -88,3 +108,8 @@ document.addEventListener('DOMContentLoaded', () => {
 		.getElementById('btnLimpiarEntrada')
 		.addEventListener('click', borrarTabla);
 });
+
+const enviarEntrada = () => {
+	//  Guardar array en local storage
+	localStorage.setItem('listaEntradas', JSON.stringify(entradas));
+};
